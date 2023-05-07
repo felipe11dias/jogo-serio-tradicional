2599a46fb7d6508ae7ed58874b1bb5f88126c7ff
Felipe Dias <felipemacdowell@unifor.br>
2023-05-07 02:21:45 -0300

    Integrando com api ranking

--- src/pages/ranking/Ranking.tsx
+++ src/pages/ranking/Ranking.tsx
@@ -1,6 +1,57 @@
 import { Pagination } from "@mui/material";
+import { useEffect, useState } from "react";
+import { listRatings } from "../../service/rest/apis/rankingRestApi";
+import { Ranking as Rank } from "../../types/Ranking";
 
 export default function Ranking() {
+  const [ratings, setRatings] = useState<Rank[]>([]);
+  const [searchRatings, setSearchRatings] = useState("");
+  const [page, setPage] = useState<number>(1);
+  const [count, setCount] = useState<number>(0);
+  
+  useEffect(() => {
+    findAllRatings()
+  }, [page])
+
+  const findAllRatings = () => {
+    const params = getRequestParams(searchRatings, page);
+
+    listRatings(params).then( data => {
+      setRatings(data.content)
+      setCount(data.totalPages)
+    }).catch( error => {
+      return null
+    })
+  }
+
+  const onChangeSearchRatings = (e: any) => {
+    const searchRatings = e.target.value;
+    setSearchRatings(searchRatings);
+  };
+
+  const getRequestParams = (searchRatings: string, page: number) => {
+    let params: any = {};
+
+    if (searchRatings) {
+      params["user"] = searchRatings;
+    }
+
+    if (page) {
+      params["page"] = page - 1;
+    }
+
+    return params;
+  };
+
+  const findByRanking = () => {
+    setPage(1);
+    findAllRatings();
+  };
+
+  const handlePageChange = (event: any, value: number) => {
+    setPage(value);
+  };
+
   return (
     <>
       <h2 className="mb-5">Classificação</h2>
@@ -9,10 +60,12 @@ export default function Ranking() {
         <div style={{ maxWidth: '500px'}}>
           <input
             type="text"
-            placeholder="Buscar por nome do aluno"
+            placeholder="Buscar por nome"
+            value={searchRatings}
+            onChange={onChangeSearchRatings}
           />
-          <button className="" >
-            Buscar por nome do aluno
+          <button className="" onClick={findByRanking}>
+            Buscar por aluno
           </button>
         </div>
       </div>
@@ -30,18 +83,36 @@ export default function Ranking() {
             </tr>
           </thead>
           <tbody>
-            
+            {
+              ratings.length > 0 ?
+              ratings.map(ranking => {
+                return (
+                  <>
+                    <td>{ranking.game}</td>
+                    <td>{ranking.user}</td>
+                    <td>{ranking.activity}</td>
+                    <td>{ranking.questionsHit}</td>
+                    <td>{ranking.time}</td>
+                    <td>{ranking.fulltime}</td>
+                  </>
+                )
+              }) :
+              <>
+                <p> Nenhuma classifição disponível </p>
+              </>
+            }
           </tbody>
         </table>
 
         <Pagination
           color="primary"
           className="my-3"
-          count={0}
-          page={0}
+          count={count}
+          page={page}
           siblingCount={1}
           boundaryCount={1}
           variant="outlined"
+          onChange={handlePageChange}
         />
 
       </div>
