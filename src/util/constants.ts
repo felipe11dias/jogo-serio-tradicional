f98c6aabffad397bc26ba9ec05b9351af82535da
Felipe Dias <felipemacdowell@unifor.br>
2023-11-12 01:21:28 -0300

    Constants base_url

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,5 +1,5 @@
 export const NOME_PROJETO = "PROJETO TCC";
-export const BASE_URL: string = "http://localhost:8080/jogos-serios-tradicionais/api/v1/"
+export const BASE_URL: string = "https://jogos-serio-tradicional-java.onrender.com/jogos-serios-tradicionais/api/v1/"
 
 export const GAME_RESPONSE_SEARCH = "response_search";
 export const GAME_AIMSHOT_SEARCH = "aimshot_search";
